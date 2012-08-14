eab04c05451c8fc7df9b5736f18bca509cd1691d
Archy Sharp <homobel@gmail.com>
2012-08-15 00:44:30 +0300

    oops again

--- ree.js
+++ ree.js
@@ -1,23 +1,11 @@
 
 //~ <component>
-//~	Name: YellowJS
-//~	Info: Color your javascript
+//~	Name: Ree Lib
+//~	Info: Working with embedded types
 //~ </component>
 
 this.ree = new function() {
 
-//~ require: type.js
-//~ require: object.js
-//~ require: array.js
-//~ require: number.js
-//~ require: string.js
-//~ require: function.js
-
-	ReeType.call(this);
-	ReeObject.call(this);
-	ReeArray.call(this);
-	ReeNumber.call(this);
-	ReeString.call(this);
-	ReeFunction.call(this);
+//~ require: ree-init.js
 
 };
